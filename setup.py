eb02f1c68f52b9c6422341145a85338c0a2444c1
Gitea <gitea@fake.local>
2022-01-31 20:45:05 +0100

    Fixed snippets

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setup(
     author='Oliver Wagner',
     author_email='oliwerix@oliwerix.com',
     license='AGPLv3.0',
-    include_package_data=True,
+    #include_package_data=True,
+    
+    packages=['nginx-generator'],
+    package_dir={'nginx-generator': 'src/nginx_generator'},
     package_data={'nginx-generator':['snippets/*']},
     install_requires=['validators','click'],
-    packages=find_packages(),
     entry_points=dict(
         console_scripts=['nginx-generator=nginx_generator.app:main']
     )
