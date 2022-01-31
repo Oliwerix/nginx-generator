c8e5eb762b45ccad1ec673dcd91ac155d024780a
Oliwerix <60409548+Oliwerix@users.noreply.github.com>
2022-01-31 20:51:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nginx-generator',
-    version='0.1.2',
+    version='0.1.3',
     description='Generate nginx configs interactivly',
     url='http://github.com/Oliwerix/nginx-generator',
     author='Oliver Wagner',
