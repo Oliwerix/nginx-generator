97c39b2aef08e0fc04401aa253a3d151d529ed9d
Gitea <gitea@fake.local>
2022-01-31 20:58:21 +0100

    a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nginx-generator',
-    version='0.1.1',
+    version='0.1.3',
     description='Generate nginx configs interactivly',
     url='http://github.com/Oliwerix/nginx-generator',
     author='Oliver Wagner',
@@ -10,9 +10,9 @@ setup(
     license='AGPLv3.0',
     #include_package_data=True,
     
-    packages=['nginx-generator'],
-    package_dir={'nginx-generator': 'src/nginx_generator'},
-    package_data={'nginx-generator':['snippets/*']},
+    packages=['nginx_generator'],
+    package_dir={'nginx_generator': 'src/nginx_generator'},
+    package_data={'nginx_generator':['snippets/*']},
     install_requires=['validators','click'],
     entry_points=dict(
         console_scripts=['nginx-generator=nginx_generator.app:main']
